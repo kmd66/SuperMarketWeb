86e13f5a11e2960a8923687c79174d12f44ddddb
mohammadi <k.mohammadi.d.1366@gmail.com>
2022-10-12 14:18:32 +0330

    .

--- Kama.Bonyad.Evaluation.WebApp/app/pages/prd/product/directive/modify/product-modify.directive.js
+++ Kama.Bonyad.Evaluation.WebApp/app/pages/prd/product/directive/modify/product-modify.directive.js
@@ -39,6 +39,7 @@
                 loadingService.show();
                 product.modify.model.Information = JSON.stringify(product.modify.Information);
                 productService.save(product.modify.model).then((result) => {
+                    product.modify.model = result
                     return product.modify.img.save(product.modify.model.GuID);
                 }).then((result) => {
                     alertService.success('جنس با موفقیت ثبت شد');
