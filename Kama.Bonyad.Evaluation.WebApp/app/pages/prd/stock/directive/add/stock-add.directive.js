fb2047002e4ed2d21e191f012403032a4291fe3f
mohammadi <k.mohammadi.d.1366@gmail.com>
2022-10-16 18:42:02 +0330

    .

--- Kama.Bonyad.Evaluation.WebApp/app/pages/prd/stock/directive/add/stock-add.directive.js
+++ Kama.Bonyad.Evaluation.WebApp/app/pages/prd/stock/directive/add/stock-add.directive.js
@@ -9,7 +9,7 @@
             link: {
                 pre: preLink
             }
-            , template: require('./stock-modify.directive.html')
+            , template: require('./stock-add.directive.html')
             , restrict: 'E'
             , scope: {
                 main: '=main'
@@ -22,38 +22,6 @@
         function preLink(scope, element, attrs) {
             let stock = scope;
 
-            stock.add.addList = addList;
-            stock.add.save = save;
-
-            function addList() {
-                stock.add.model.ID = $routeParams.id;
-                loadingService.show();
-                stockService.save(stock.add.model).then((result) => {
-                    return stockService.get({ ID: stock.add.model.ID });
-                }).then((result) => {
-                    stock.modify.model = result;
-                    stock.modify.grid.items = stock.modify.model;
-                    stock.add.showAdd = false;
-                    stock.add.model = {}
-                    alertService.success('جنس با موفقیت ثبت شد');
-                }).catch(alertService.error).finally(loadingService.hide);
-
-            }
-
-            function save() {
-                stock.add.model.ID = $routeParams.id;
-                loadingService.show();
-                stockService.save(stock.add.model).then((result) => {
-                    return stockService.get({ ID: stock.add.model.ID });
-                }).then((result) => {
-                    stock.modify.model = result;
-                    stock.modify.grid.items = stock.modify.model;
-                    stock.add.showAdd = false;
-                    stock.add.model = {}
-                    alertService.success('جنس با موفقیت ثبت شد');
-                }).catch(alertService.error).finally(loadingService.hide);
-
-            }
 
         }
     }
