5b25b623a769fc111189f2400ff389dcacb9e3da
kamatech-ir <98743650+kamatech-ir@users.noreply.github.com>
2023-02-06 12:13:14 +0330

    prodact delete

---
+++ Kama.Bonyad.Evaluation.WebApp/app/pages/prd/product2/directive/cartable/product-cartable.directive.js
@@ -0,0 +1,62 @@
+﻿//(() => {
+//    angular
+//        .module('evaluation')
+//        .directive('kamaProductCartable', kamaProductCartable);
+
+//    kamaProductCartable.$inject = ['globalService', 'productClassificationService', 'productService'];
+//    function kamaProductCartable(globalService, productClassificationService, productService) {
+//        let directive = {
+//            link: {
+//                pre: preLink
+//            }
+//            , template: require('./product-cartable.directive.html')
+//            , restrict: 'E'
+//            , scope: {
+//                main: '=main'
+//                , cartable: '=cartable'
+//                , search: '=search'
+//            }
+//        };
+
+//        return directive;
+
+//        function preLink(scope, element, attrs) {
+//            let product = scope;
+
+//            product.cartable.resetSearch = resetSearch;
+//            resetSearch();
+
+//            product.cartable.grid = {
+//                bindingObject: product.cartable
+//                , columns: [
+//                    { name: 'Name', displayName: 'نام' }
+//                    , { name: 'ClassificationName', displayName: 'نام دسته' }
+//                    , { name: 'Comment', displayName: 'توضیح' }
+//                ]
+//                , initload: false
+//                , pageSize: 100//globalService.get('userSettings').PageSize
+//                , options: () => { return product.search.model; }
+//                , listService: productService.list
+//                , onAdd: product.main.changeState.add
+//                , onEdit: product.main.changeState.edit
+//                , deleteService: productService.remove
+//            };
+
+//            product.search.parentProductDropdown = {
+//                bindingObject: product.search
+//                , parameters: { ID: 'ParentID' }
+//                , select2: true
+//                //, initLoad: true
+//                //, listService: productService.list
+//            };
+
+//            function resetSearch() {
+//                product.search.model.StartPrice = 0;
+//                product.search.model.EndPrice = 0;
+//                product.search.pageIndex = 1;
+//                product.search.state = 'hide';
+//            }
+
+//        }
+//    }
+//})();
---
+++ Kama.Bonyad.Evaluation.WebApp/app/pages/prd/product2/directive/modify/product-modify.directive.js
@@ -0,0 +1,93 @@
+﻿//(() => {
+//    angular
+//        .module('evaluation')
+//        .directive('kamaProductModify', kamaProductModify);
+
+//    kamaProductModify.$inject = ['productService', 'loadingService', 'alertService', 'enumService', 'productClassificationService', 'brandService', '$q'];
+//    function kamaProductModify(productService, loadingService, alertService, enumService, productClassificationService, brandService, $q) {
+//        let directive = {
+//            link: {
+//                pre: preLink
+//            }
+//            , template: require('./product-modify.directive.html')
+//            , restrict: 'E'
+//            , scope: {
+//                main: '=main'
+//                , modify: '=modify'
+//                , cartable: '=cartable'
+//                , tag: '=tag'
+//            }
+//        };
+
+//        return directive;
+//        function preLink(scope, element, attrs) {
+//            let product = scope;
+
+//            product.modify.save = save;
+//            product.modify.img = {
+//                Type: 2,
+//                hideHead: true
+//            };
+//            product.modify.unitOfMeasureTypeDropDown = {
+//                items: enumService.UnitOfMeasureType
+//                , bindingObject: product.modify
+//                , parameters: { ID: 'UnitOfMeasure', Name: 'UnitOfMeasureName' }
+//            };
+
+//            product.modify.filterParentProductDropdown = {
+//                bindingObject: product.modify
+//                , parameters: { GuID: 'FilterParentID', ID: 'FilterParentIntID' }
+//                , select2: true
+//                , onChange: (selected, props) => {
+//                    if (!props.isEmpty) {
+//                        loadingService.show();
+//                        return $q.resolve().then(() => {
+//                            return product.modify.parentProductDropdown.getlist();
+//                        }).then(() => {
+//                            return product.modify.berandDropdown.getlist();
+//                        }).finally(loadingService.hide);
+//                    }
+//                }
+//            };
+
+//            product.modify.parentProductDropdown = {
+//                bindingObject: product.modify
+//                , parameters: { ID: 'ParentID' }
+//                , select2: true
+//                , options: () => {
+//                    if (product.modify.model.FilterParentID)
+//                        return { ParentID: product.modify.model.FilterParentID }
+//                    else
+//                        return { LastNode: true }
+//                }
+//                , listService: productClassificationService.list
+//            };
+
+//            product.modify.berandDropdown = {
+//                bindingObject: product.modify
+//                , parameters: { ID: 'BrandID' }
+//                , select2: true
+//                , listService: () => {
+//                    return brandService.list({ ParentID: product.modify.model.FilterParentIntID1 });
+//                }
+//            };
+
+//            function save(getCartable) {
+//                loadingService.show();
+//                product.modify.model.Information = JSON.stringify(product.modify.Information);
+//                productService.save(product.modify.model).then((result) => {
+//                    product.modify.model = result
+//                    return product.modify.img.save(product.modify.model.GuID);
+//                }).then((result) => {
+//                    alertService.success('جنس با موفقیت ثبت شد');
+//                    if (getCartable) {
+//                        product.main.changeState.cartable();
+//                        return product.cartable.grid.getlist(false);
+//                    }
+//                }).catch(alertService.error).finally(loadingService.hide);
+
+//            }
+
+//        }
+//    }
+//})();
